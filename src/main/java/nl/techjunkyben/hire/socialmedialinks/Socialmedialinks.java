8c7076afaa42d3e414ad473cf623e76fec0d6b89
BuildTools <unconfigured@null.spigotmc.org>
2023-10-30 16:55:10 +0100

    Initial commit

---
+++ src/main/java/nl/techjunkyben/hire/socialmedialinks/Socialmedialinks.java
@@ -0,0 +1,17 @@
+package nl.techjunkyben.hire.socialmedialinks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Socialmedialinks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
